90e8b7a0ea054881fc8680a8f515f541d0d2004d
kyas <kyas@29972ad9-4e1c-0410-bd41-baaa3728e1af>
2007-01-31 12:12:23 +0000

    Added another test case.  Implemented a crude translation to Maude.
    git-svn-id: https://svn.ifi.uio.no/repos/projects/credo/trunk/creolcomp@215 29972ad9-4e1c-0410-bd41-baaa3728e1af

--- Creol.ml
+++ Creol.ml
@@ -1,3 +1,26 @@
+(*
+ * Creol.ml -- Definition and manipulation of Creol AST
+ *
+ * This file is part of creolcomp
+ *
+ * Written and Copyright (c) 2007 by Marcel Kyas
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *)
+
 (** The abstract syntax of the (unspecified) Creol expression language or
     functional sub-language *)
 type expression =
@@ -143,3 +166,88 @@ let rec pretty_print out_channel =
       [] -> flush out_channel
     | Class c::l -> pretty_print_class out_channel c; pretty_print out_channel l
     | Interface i::l -> pretty_print_iface out_channel i; pretty_print out_channel l
+
+
+(* Write a Creol program as a maude term. If the program is parsable
+   but not semantically correct, this function will only produce
+   garbage. *)
+
+let rec maude_of_creol_expression out =
+  function
+      Int i -> output_string out ("int(" ^ (string_of_int i) ^ ")")
+    | Float f -> ()
+    | Bool false -> output_string out "bool(false)"
+    | Bool true -> output_string out "bool(true)"
+    | Id i -> output_string out ("'" ^ i)
+    | Unary (o, e) ->
+	output_string out "(";
+	output_string out (match o with Not ->  "not" | UMinus -> "-");
+	maude_of_creol_expression out e;
+	output_string out ")"
+    | Binary (o, l, r) ->
+	output_string out "("; maude_of_creol_expression out l;
+	output_string out (match o with
+	    Plus -> " + "
+	  | Minus -> " - "
+	  | Times -> " * "
+	  | Div -> " / "); 
+	maude_of_creol_expression out r; output_string out ")"
+
+let rec maude_of_creol_guard out =
+  function
+      Label l -> ()
+    | Condition c -> maude_of_creol_expression out c
+    | Conjunction (l, r) -> maude_of_creol_guard out l;
+	output_string out " 'and "; maude_of_creol_guard out r
+
+let rec maude_of_creol_statement out =
+  function
+      Skip -> ()
+    | Await g -> output_string out "await "; maude_of_creol_guard out g
+    | If (c, t, Skip) ->
+	output_string out "if "; maude_of_creol_expression out c;
+	output_string out " th "; maude_of_creol_statement out t;
+	output_string out " fi"
+    | If (c, t, f) ->
+	output_string out "if "; maude_of_creol_expression out c;
+	output_string out " th "; maude_of_creol_statement out t;
+	output_string out " el "; maude_of_creol_statement out f;
+	output_string out " fi"
+    | Sequence (l, r) ->
+	output_string out "( "; maude_of_creol_statement out l; 
+	output_string out " ; "; maude_of_creol_statement out r; 
+	output_string out " )"
+    | Merge (l, r) ->
+	output_string out "( "; maude_of_creol_statement out l; 
+	output_string out " ||| "; maude_of_creol_statement out r; 
+	output_string out " )"
+    | Choice (l, r) ->
+	output_string out "( "; maude_of_creol_statement out l; 
+	output_string out " [] "; maude_of_creol_statement out r; 
+	output_string out " )"
+
+let maude_of_creol_attribute out a =
+  output_string out ("(" ^ a.var_name ^ ": ");
+  (match a.var_init with
+      None -> output_string out "null"
+    | Some e -> maude_of_creol_expression out e);
+  output_string out ")"
+
+let rec maude_of_creol_attributes out =
+  function
+      [] ->  output_string out "none"
+    | [a] -> maude_of_creol_attribute out a
+    | a::l -> maude_of_creol_attribute out a; output_string out ", ";
+	maude_of_creol_attributes out l
+
+let maude_of_creol_class out c =
+  output_string out ("< \'" ^ c.cls_name ^ " : Cl | Att: ");
+  output_string out ">"
+
+let maude_of_creol_interface out i = ()
+
+let rec maude_of_creol out =
+  function
+      [] -> flush out
+    | Class c::l -> maude_of_creol_class out c; maude_of_creol out l
+    | Interface i::l -> maude_of_creol out l (* they dont exist in Maude *)
