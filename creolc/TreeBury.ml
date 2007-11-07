09b4664f3f55caf3cc62c05fd57beac0df9e039e
kyas <kyas@29972ad9-4e1c-0410-bd41-baaa3728e1af>
2007-11-07 15:14:14 +0000

    First attempt at burying variables.
    git-svn-id: https://svn.ifi.uio.no/repos/projects/credo/trunk/creoltools@1051 29972ad9-4e1c-0410-bd41-baaa3728e1af

--- creolc/TreeBury.ml
+++ creolc/TreeBury.ml
@@ -1,5 +1,5 @@
 (*
- * TreeDeadvar.ml -- Free or bury dead variables.
+ * TreeBury.ml -- Bury dead variables.
  *
  * This file is part of creoltools
  *
@@ -19,36 +19,70 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
-(*s Free and bury variables holding dead values.
+(*s Bury all variables holding dead values.
 
-*)
+  Requires life ranges. *)
 
 open Creol
 open Expression
 open Statement
+open VarDecl
+open Method
+
+(* This function inserts bury statements for all variables at the
+   point at which they die.
+
+   \textbf{TO DO:} This function will often insert redundant bury
+   statements.  The user may write \lstinline!v := null! and never
+   read from \lstinline!v! thereafter.  Then \lstinline!v! is declared
+   to be dead and a [Bury] node is generated, causing an additional
+   \lstinline!v := null! in the output. *)
 
 let optimize prg =
-  (** Take a program and try to replace tail calls with a version using
-      out special macro. *)
-  let rec optimise_declaration =
-    function
-      Declaration.Class c -> Declaration.Class (optimise_in_class c)
-    | Declaration.Interface i -> Declaration.Interface i
-    | Declaration.Exception e -> Declaration.Exception e
-    | Declaration.Datatype d -> Declaration.Datatype d
-    | Declaration.Function f -> Declaration.Function f
-  and optimise_in_class c =
-    { c with Class.with_defs = List.map optimise_in_with c.Class.with_defs }
-  and optimise_in_with w =
-    { w with With.methods = List.map optimise_in_method w.With.methods }
-  and optimise_in_method m =
-    match m.Method.body with
-	None -> m
+  let optimise_in_statement meth stmt =
+    let rec work =
+      function
+	  Sequence (n, s1, s2) ->
+	    let s1' = work s1 and s2' = work s2 in
+	    let k =
+	      IdSet.filter
+		(fun v -> not (Type.label_p (find_variable meth v).var_type))
+		(IdSet.diff (life s1) (life s2))
+	    in
+	      if IdSet.is_empty k then
+		Sequence (n, s1', s2')
+	      else
+		let k' =
+		  IdSet.fold
+		    (fun e a -> (LhsId (Expression.dummy_note, e))::a)
+		    k []
+		in
+		  Sequence (n, s1',
+			   Sequence (note s2, Bury (note s2, k'), s2'))
+	| Choice (n, s1, s2) ->
+	    Choice (n, work s1, work s2)
+	| Merge (n, s1, s2) ->
+	    Merge (n, work s1, work s2)
+	| s -> s
+    in
+      work stmt
+  in
+  let optimise_in_method meth =
+    match meth.Method.body with
+	None ->
+	  meth
       | Some body ->
-	  { m with Method.body =
-	      Some ((optimise_in_statement
-			(List.map (function v -> v.VarDecl.name) m.Method.outpars))
-		       body) } 
-  and optimise_in_statement outs s = s
+	  { meth with Method.body = Some (optimise_in_statement meth body) }
+  in
+  let optimise_in_with w =
+    { w with With.methods = List.map optimise_in_method w.With.methods }
+  in
+  let optimise_in_class c =
+    { c with Class.with_defs = List.map optimise_in_with c.Class.with_defs }
+  in
+  let optimise_declaration =
+    function
+	Declaration.Class c -> Declaration.Class (optimise_in_class c)
+      | d -> d
   in
     List.map optimise_declaration prg
