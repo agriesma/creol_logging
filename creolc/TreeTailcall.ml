78b2620e507b021a9f65e19a99d4249bae340673
kyas <kyas@29972ad9-4e1c-0410-bd41-baaa3728e1af>
2008-09-13 15:19:54 +0000

    Reinstate and improve implementation of tailcall optimisations.
    git-svn-id: https://svn.ifi.uio.no/repos/projects/credo/trunk/creoltools@1686 29972ad9-4e1c-0410-bd41-baaa3728e1af

--- creolc/TreeTailcall.ml
+++ creolc/TreeTailcall.ml
@@ -31,7 +31,68 @@ let tailcall_counter = ref 0
 
 let tailcall_successes () = !tailcall_counter
 
-let optimise_in_statement outs stmt = stmt
+let optimise_in_statement outs stmt =
+  let fits_p out expr =
+    match expr with
+      | LhsId (_, n) | LhsSSAId (_, n, _) when n = out -> true
+      | _ -> false
+  and matches_p out expr =
+    match expr with
+      | Id (_, n) | SSAId (_, n, _) when n = out -> true
+      | _ -> false
+  and equals_p out expr =
+    match (out, expr) with
+      | (LhsId(_, n), Id(_, n'))
+      | (LhsSSAId(_, n, _), Id(_, n'))
+      | (LhsId(_, n), SSAId(_, n', _)) -> n = n'
+      | (LhsSSAId(_, n, i), SSAId(_, n', i')) -> n = n' && i = i'
+      | _ -> false
+  in
+  let rec work =
+    function
+      | Sequence (n, SyncCall (_, c, m, s, i, o), Return (_, r)) when
+	      ((List.length outs) = (List.length o)) &&
+	      ((List.length outs) = (List.length r)) &&
+	      (List.for_all2 fits_p outs o) &&
+	      (List.for_all2 matches_p outs r) ->
+	  Tailcall (n, c, m, s, i)
+      | Sequence (n, AsyncCall (_, Some l, c, m, s, i),
+	  Sequence (_, Reply(_, l', o), Return (_, r))) when
+	      (equals_p l l') &&
+	      ((List.length outs) = (List.length o)) &&
+	      ((List.length outs) = (List.length r)) &&
+	      (List.for_all2 fits_p outs o) &&
+	      (List.for_all2 matches_p outs r) ->
+	  Tailcall (n, c, m, s, i)
+      | Sequence (n, LocalSyncCall (_, m, s, lb, ub, i, o), Return (_, r)) when
+	      ((List.length outs) = (List.length o)) &&
+	      ((List.length outs) = (List.length r)) &&
+	      (List.for_all2 fits_p outs o) &&
+	      (List.for_all2 matches_p outs r) ->
+	  StaticTail (n, m, s, lb, ub, i)
+      | Sequence (n, LocalAsyncCall (_, Some l, m, s, lb, ub, i),
+	  Sequence (_, Reply(_, l', o), Return (_, r))) when
+	      (equals_p l l') &&
+	      ((List.length outs) = (List.length o)) &&
+	      ((List.length outs) = (List.length r)) &&
+	      (List.for_all2 fits_p outs o) &&
+	      (List.for_all2 matches_p outs r) ->
+	  StaticTail (n, m, s, lb, ub, i)
+      | If (n, c, s1, s2) ->
+	  If (n, c, work s1, work s2)
+      | While (n, c, i, s1) ->
+	  While (n, c, i, work s1)
+      | DoWhile (n, c, i, s1) ->
+	  DoWhile (n, c, i, work s1)
+      | Sequence (n, s1, s2) ->
+	  Sequence (n, work s1, work s2)
+      | Choice (n, s1, s2) ->
+	  Choice (n, work s1, work s2)
+      | Merge (n, s1, s2) ->
+	  Merge (n, work s1, work s2)
+      | s -> s
+  in
+    work stmt
 
 let optimise_in_method prg cls m =
   match m.Method.body with
