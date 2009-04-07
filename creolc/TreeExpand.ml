b19a854d2877cd0b228612ba5d369db36ebbd451
Marcel Kyas <marcel.kyas@fu-berlin.de>
2009-04-07 14:36:13 +0200

    Refactored with the goal to check that a freshly generated name is really a fresh name that we can use in a method.

--- creolc/TreeExpand.ml
+++ creolc/TreeExpand.ml
@@ -65,226 +65,235 @@ let initialiser ?(cls="") vdecls =
     remove_redundant_skips s
 
 
-(* Lower a Creol program to the "Core Creol" language.
-   
-   This function will destroy some statement and expression
-   annotations.  Therefore, all semantic analysis performed before
-   this function should be repeated after calling this function.
-
-   This should only concern type inference, because all other
-   analysis should be performed after this function.
-
-   The following two invariant holds for this function:
-   \begin{itemize}
-   \item A type correct program remains type correct and the
-     annotations of unchanged statements are the same after
-     reconstruction.
-   \item [expand (expand tree) == expand tree]
-   \end{itemize}
-*)
-let pass input =
+let rec expand_statement cls mtd future_decls fresh_name =
   let future_decl l t =
     { VarDecl.name = l; var_type = t; init = None; file = ""; line = 0 }
   in
-  let rec expand_statement future_decls fresh_name =
-    function
-      | Skip _ | Release _ | Assert _ | Prove _ | Assign _ | Await _
-      | Posit _  as s ->
-          (future_decls, fresh_name, s)
-      | AsyncCall (a, None, e, n, ((co, dom, Some rng) as s), p) ->
-	  (* If a future name is not given, we assign a new one and
-	     free it afterwards. *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future rng in
-	  let n' = make_expr_note_from_stmt_note a lt in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	     AsyncCall (a, Some (LhsId (n', l)), e, n, s, p))
-      | AsyncCall (a, None, e, n, s, p) ->
-          if not (Type.collection_p (Expression.get_type e)) then
-	    (* If a future name is not given, we create a new future name.
-               We cannot give a correct type to the future.  If the type
-               checker is run after expanding, it may report errors for
-               this call.  *)
-            begin
-	      let Misc.FreshName (l, fresh_name') = fresh_name () in
-	      let lt = Type.future [] in
-	      let a' = make_expr_note_from_stmt_note a lt in
-	        ((future_decl l lt)::future_decls, fresh_name',
-	         AsyncCall (a, Some (LhsId (a', l)), e, n, s, p))
-	    end
-	  else
-	    (* The type checker inferred that the callee expression refers
-	       to a collection type.  In this case, we convert to a
-	       MultiCast statement. *)
-	    (future_decls, fresh_name, MultiCast (a, e, n, s, p))
-      | AsyncCall _ | Free _ | Bury _ | Get _ as s ->
-          (future_decls, fresh_name, s)
-      | SyncCall (a, e, n, s, p, r) ->
-	  (* Replace the synchronous call by the sequence of an asynchronous
-	     call followed by a reply.  This generates a fresh future name.  *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future (List.map get_lhs_type r) in
-	  let a' = make_expr_note_from_stmt_note a lt in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	    Sequence (a, AsyncCall (a, Some (LhsId (a', l)), e, n, s, p),
+  let rec make fn =
+    let Misc.FreshName (l, fn') = fn () in
+      (l, fn')
+  in
+  function
+    | Skip _ | Release _ | Assert _ | Prove _ | Assign _ | Await _
+    | Posit _  as s ->
+        (future_decls, fresh_name, s)
+    | AsyncCall (a, None, e, n, ((co, dom, Some rng) as s), p) ->
+	(* If a future name is not given, we assign a new one and
+	   free it afterwards. *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future rng in
+	let n' = make_expr_note_from_stmt_note a lt in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   AsyncCall (a, Some (LhsId (n', l)), e, n, s, p))
+    | AsyncCall (a, None, e, n, s, p) ->
+        if not (Type.collection_p (Expression.get_type e)) then
+	  (* If a future name is not given, we create a new future name.
+             We cannot give a correct type to the future.  If the type
+             checker is run after expanding, it may report errors for
+             this call.  *)
+          begin
+	    let (l, fresh_name') = make fresh_name in
+	    let lt = Type.future [] in
+	    let a' = make_expr_note_from_stmt_note a lt in
+	      ((future_decl l lt)::future_decls, fresh_name',
+	       AsyncCall (a, Some (LhsId (a', l)), e, n, s, p))
+	  end
+	else
+	  (* The type checker inferred that the callee expression refers
+	     to a collection type.  In this case, we convert to a
+	     MultiCast statement. *)
+	  (future_decls, fresh_name, MultiCast (a, e, n, s, p))
+    | AsyncCall _ | Free _ | Bury _ | Get _ as s ->
+        (future_decls, fresh_name, s)
+    | SyncCall (a, e, n, s, p, r) ->
+	(* Replace the synchronous call by the sequence of an asynchronous
+	   call followed by a reply.  This generates a fresh future name.  *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future (List.map get_lhs_type r) in
+	let a' = make_expr_note_from_stmt_note a lt in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   Sequence (a, AsyncCall (a, Some (LhsId (a', l)), e, n, s, p),
 		     Get (a, Id (a', l), r)))
-      | AwaitSyncCall (a, e, n, s, p, r) ->
-	  (* Replace the synchronous call by the sequence of an asynchronous
-	     call followed by a reply.  This generates a fresh future name.  *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future (List.map get_lhs_type r)
-	  in
-	  let a' = make_expr_note_from_stmt_note a lt
-	  and a'' = make_expr_note_from_stmt_note a Type.bool
-	  in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	    Sequence (a, AsyncCall (a, Some (LhsId (a', l)), e, n, s, p),
+    | AwaitSyncCall (a, e, n, s, p, r) ->
+	(* Replace the synchronous call by the sequence of an asynchronous
+	   call followed by a reply.  This generates a fresh future name.  *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future (List.map get_lhs_type r)
+	in
+	let a' = make_expr_note_from_stmt_note a lt
+	and a'' = make_expr_note_from_stmt_note a Type.bool
+	in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   Sequence (a, AsyncCall (a, Some (LhsId (a', l)), e, n, s, p),
 		     Sequence(a, Await (a, Label (a'', Id (a', l))),
-			     Get (a, Id (a', l), r))))
-      | LocalAsyncCall (a, None, m, ((c, dom, Some rng) as s), lb, ub, i) ->
-	  (* If a future name is not given, we assign a new one and free it
-	     afterwards. *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future rng in
-	  let a' = make_expr_note_from_stmt_note a lt in
-	    ((future_decl l (Type.future rng))::future_decls, fresh_name',
-	     LocalAsyncCall(a, Some (LhsId (a', l)), m, s, lb, ub, i))
-      | LocalAsyncCall (a, None, m, s, lb, ub, i) ->
-	  (* If a future name is not given, we create a new future name
-	     and assign the call to it.  We cannot give a correct type
-	     to the future.  If the type checker is run after expanding,
-	     we may see a type error since there is no corresponding
-	     method declared.  *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name () in
-	  let lt = Type.future [] in
-	  let a' = make_expr_note_from_stmt_note a lt in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	     LocalAsyncCall(a, Some (LhsId (a', l)), m, s, lb, ub, i))
-      | LocalAsyncCall _ as s ->
-	    (future_decls, fresh_name, s)
-      | LocalSyncCall (a, m, s, lb, ub, i, o) ->
-	  (* Replace the synchronous call by the sequence of an asynchronous
-	     call followed by a reply.  This generates a fresh future name.  *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future (List.map get_lhs_type o)
-	  in
-	  let a' = make_expr_note_from_stmt_note a lt in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	    Sequence (a, LocalAsyncCall (a, Some (LhsId (a', l)), m, s, lb, ub, i),
+			      Get (a, Id (a', l), r))))
+    | LocalAsyncCall (a, None, m, ((c, dom, Some rng) as s), lb, ub, i) ->
+	(* If a future name is not given, we assign a new one and free it
+	   afterwards. *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future rng in
+	let a' = make_expr_note_from_stmt_note a lt in
+	  ((future_decl l (Type.future rng))::future_decls, fresh_name',
+	   LocalAsyncCall(a, Some (LhsId (a', l)), m, s, lb, ub, i))
+    | LocalAsyncCall (a, None, m, s, lb, ub, i) ->
+	(* If a future name is not given, we create a new future name
+	   and assign the call to it.  We cannot give a correct type
+	   to the future.  If the type checker is run after expanding,
+	   we may see a type error since there is no corresponding
+	   method declared.  *)
+	let (l, fresh_name') = make fresh_name in
+	let lt = Type.future [] in
+	let a' = make_expr_note_from_stmt_note a lt in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   LocalAsyncCall(a, Some (LhsId (a', l)), m, s, lb, ub, i))
+    | LocalAsyncCall _ as s ->
+	(future_decls, fresh_name, s)
+    | LocalSyncCall (a, m, s, lb, ub, i, o) ->
+	(* Replace the synchronous call by the sequence of an asynchronous
+	   call followed by a reply.  This generates a fresh future name.  *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future (List.map get_lhs_type o)
+	in
+	let a' = make_expr_note_from_stmt_note a lt in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   Sequence (a, LocalAsyncCall (a, Some (LhsId (a', l)), m, s, lb, ub, i),
 		     Get (a, Id (a', l), o)))
-      | AwaitLocalSyncCall (a, m, s, lb, ub, i, o) ->
-	  (* Replace the synchronous call by the sequence of an asynchronous
-	     call followed by a reply.  This generates a fresh future name.  *)
-	  let Misc.FreshName (l, fresh_name') = fresh_name ()
-	  and lt = Type.future (List.map get_lhs_type o)
-	  in
-	  let a' = make_expr_note_from_stmt_note a lt 
-	  and a'' = make_expr_note_from_stmt_note a Type.bool in
-	    ((future_decl l lt)::future_decls, fresh_name',
-	    Sequence (a, LocalAsyncCall (a, Some (LhsId (a', l)), m, s, lb, ub, i),
+    | AwaitLocalSyncCall (a, m, s, lb, ub, i, o) ->
+	(* Replace the synchronous call by the sequence of an asynchronous
+	   call followed by a reply.  This generates a fresh future name.  *)
+	let (l, fresh_name') = make fresh_name
+	and lt = Type.future (List.map get_lhs_type o)
+	in
+	let a' = make_expr_note_from_stmt_note a lt 
+	and a'' = make_expr_note_from_stmt_note a Type.bool in
+	  ((future_decl l lt)::future_decls, fresh_name',
+	   Sequence (a, LocalAsyncCall (a, Some (LhsId (a', l)), m, s, lb, ub, i),
 		     Sequence (a, Await (a, Label(a'', Id (a', l))),
-			      Get (a, Id (a', l), o))))
-      | MultiCast _ | Tailcall _ | StaticTail _ as s ->
-            (future_decls, fresh_name, s)
-      | If (a, c, t, f) ->
-	  let (future_decls', fresh_name', t') =
-            expand_statement future_decls fresh_name t
-          in
-	  let (future_decls'', fresh_name'', f') =
-            expand_statement future_decls' fresh_name' f
-          in
-	    (future_decls'', fresh_name'', If(a, c, t', f'))
-      | While (a, c, i, b) ->
-	  let (future_decls', fresh_name', b') =
-            expand_statement future_decls fresh_name b
-          in
-	    (future_decls', fresh_name', While (a, c, i, b'))
-      | DoWhile (a, c, i, b) ->
-	  expand_statement future_decls fresh_name
-            (Sequence (a, b, While (a, c, i, b)))
-      | Sequence (a, s1, s2) ->
-	  let (future_decls', fresh_name', s1') =
-            expand_statement future_decls fresh_name s1
-          in
-	  let (future_decls'', fresh_name'', s2') =
-            expand_statement future_decls' fresh_name' s2
-          in
-	    (future_decls'', fresh_name'', Sequence (a, s1', s2'))
-      | Merge (a, s1, s2) ->
-	  let (future_decls', fresh_name', s1') =
-            expand_statement future_decls fresh_name s1
-          in
-	  let (future_decls'', fresh_name'', s2') =
-            expand_statement future_decls' fresh_name' s2
-          in
-	    (future_decls'', fresh_name'', Merge (a, s1', s2'))
-      | Choice (a, s1, s2) ->
-	  let (future_decls', fresh_name', s1') =
-            expand_statement future_decls fresh_name s1
-          in
-	  let (future_decls'', fresh_name'', s2') =
-            expand_statement future_decls' fresh_name' s2
-          in
-	    (future_decls'', fresh_name'', Choice (a, s1', s2'))
-      | Return _ | Continue _ | Extern _ as s ->
-          (future_decls, fresh_name, s)
-  and expand_method_variables note vars =
+			       Get (a, Id (a', l), o))))
+    | MultiCast _ | Tailcall _ | StaticTail _ as s ->
+        (future_decls, fresh_name, s)
+    | If (a, c, t, f) ->
+	let (future_decls', fresh_name', t') =
+          expand_statement cls mtd future_decls fresh_name t
+        in
+	let (future_decls'', fresh_name'', f') =
+          expand_statement cls mtd future_decls' fresh_name' f
+        in
+	  (future_decls'', fresh_name'', If(a, c, t', f'))
+    | While (a, c, i, b) ->
+	let (future_decls', fresh_name', b') =
+          expand_statement cls mtd future_decls fresh_name b
+        in
+	  (future_decls', fresh_name', While (a, c, i, b'))
+    | DoWhile (a, c, i, b) ->
+	expand_statement cls mtd future_decls fresh_name
+          (Sequence (a, b, While (a, c, i, b)))
+    | Sequence (a, s1, s2) ->
+	let (future_decls', fresh_name', s1') =
+          expand_statement cls mtd future_decls fresh_name s1
+        in
+	let (future_decls'', fresh_name'', s2') =
+          expand_statement cls mtd future_decls' fresh_name' s2
+        in
+	  (future_decls'', fresh_name'', Sequence (a, s1', s2'))
+    | Merge (a, s1, s2) ->
+	let (future_decls', fresh_name', s1') =
+          expand_statement cls mtd future_decls fresh_name s1
+        in
+	let (future_decls'', fresh_name'', s2') =
+          expand_statement cls mtd future_decls' fresh_name' s2
+        in
+	  (future_decls'', fresh_name'', Merge (a, s1', s2'))
+    | Choice (a, s1, s2) ->
+	let (future_decls', fresh_name', s1') =
+          expand_statement cls mtd future_decls fresh_name s1
+        in
+	let (future_decls'', fresh_name'', s2') =
+          expand_statement cls mtd future_decls' fresh_name' s2
+        in
+	  (future_decls'', fresh_name'', Choice (a, s1', s2'))
+    | Return _ | Continue _ | Extern _ as s ->
+        (future_decls, fresh_name, s)
 
-    (* Compute a pair of a new list of local variable declarations and
-       a list of assignments used for initialisation.
 
-       if the variable list is empty or no variable in the list has an
-       initializer, this function will produce a skip statement as the
-       method-call's initialization.  The caller of this function should
-       check for this and discard the initalization block.
 
-       The initialisation component of variable declarations will be
-       removed. *)
+(** Compute a pair of a new list of local variable declarations and a
+    list of assignments used for initialisation.
 
-    let expand_method_variable =
-      function 
-          ({ VarDecl.name = n ; var_type = _ ; init = Some i } as v) ->
-	    ([{ v with VarDecl.init = None }],
-	    Assign(note, [LhsId(Expression.note i, n)], [i]))
-        | v -> ([v], Skip note)
-    in
-      match vars with
-	  [] -> ([], Skip note)
-	| [v] -> (expand_method_variable v)
-	| v::l ->
-	    let vl = expand_method_variable v
-	    and ll = expand_method_variables note l in
-	      match vl with
-		  (vll, Assign _) -> (vll@(fst ll), Sequence(note, (snd vl), (snd ll)))
-		| (vll, Skip _) -> (vll@(fst ll), (snd ll))
-		| _ -> assert false
-  and expand_method m =
+    if the variable list is empty or no variable in the list has an
+    initializer, this function will produce a skip statement as the
+    method-call's initialization.  The caller of this function should
+    check for this and discard the initalization block.
+
+    The initialisation component of variable declarations will be
+    removed. *)
+let rec expand_method_variables note vars =
+  let expand_method_variable = function 
+      ({ VarDecl.name = n ; var_type = _ ; init = Some i } as v) ->
+	([{ v with VarDecl.init = None }],
+	 Assign(note, [LhsId(Expression.note i, n)], [i]))
+    | v -> ([v], Skip note)
+  in
+    match vars with
+	[] -> ([], Skip note)
+      | [v] -> (expand_method_variable v)
+      | v::l ->
+	  let vl = expand_method_variable v
+	  and ll = expand_method_variables note l in
+	    match vl with
+		(vll, Assign _) -> (vll@(fst ll), Sequence(note, (snd vl), (snd ll)))
+	      | (vll, Skip _) -> (vll@(fst ll), (snd ll))
+	      | _ -> assert false
+
+
+
+let expand_method cls mtd =
     (** Simplify a method definition. *)
-    match m.Method.body with
-      | None -> m
+    match mtd.Method.body with
+      | None -> mtd
       | Some mb  ->
 	  let (future_decls, _, mb') =
             let fresh_name = Misc.fresh_name_gen "label:" in
-              expand_statement [] fresh_name mb
+              expand_statement cls mtd [] fresh_name mb
 	  and outs =
 	    List.map
 	      (fun { VarDecl.name = n } -> (Id (Expression.make_note (), n)))
-	      m.Method.outpars
+	      mtd.Method.outpars
 	  in
 	  let mb'' = Sequence (Statement.note mb, mb',
 			       Return (Statement.note mb, outs))
 	  and (vars', init) =
 	    expand_method_variables (Statement.note mb)
-	      (future_decls @ m.Method.vars)
+	      (future_decls @ mtd.Method.vars)
 	  in
-	    { m with Method.vars = vars' ;
-	      body = Some (if Statement.skip_p init then
-		  normalize_sequences mb''
-		else
-		  normalize_sequences
-		    (Sequence (Statement.note mb, init, mb''))) }
-  and expand_with w =
-    { w with With.methods = List.map expand_method w.With.methods }
+	    { mtd with Method.vars = vars' ;
+		body = Some (if Statement.skip_p init then
+			       normalize_sequences mb''
+			     else
+			       normalize_sequences
+				 (Sequence (Statement.note mb, init, mb''))) }
+
+
+(** Lower a Creol program to the "Core Creol" language.
+    
+    1This function will destroy some statement and expression
+    annotations.  Therefore, all semantic analysis performed before
+    this function should be repeated after calling this function.
+
+    This should only concern type inference, because all other
+    analysis should be performed after this function.
+
+    The following two invariant holds for this function:
+    \begin{itemize}
+    \item A type correct program remains type correct and the
+    annotations of unchanged statements are the same after
+    reconstruction.
+    \item [expand (expand tree) == expand tree]
+    \end{itemize}
+*)
+let pass input =
+  let rec expand_with cls w =
+    { w with With.methods = List.map (expand_method cls) w.With.methods }
 
   (* Rewrite the class into a expanded form.  This entails expanding of
      all sub-parts of the class, but also moving the direct initialisation
@@ -409,7 +418,7 @@ let pass input =
 	 not yet expanded to normal form. *)
 
       { c with Class.attributes = a';
-	with_defs = List.map expand_with (add_init_and_run with_defs') }
+	with_defs = List.map (expand_with c) (add_init_and_run with_defs') }
   and expand_interface i =
     i
   and expand_declaration =
