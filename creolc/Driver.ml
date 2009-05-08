71f772b5ae2975ae93d14625f7c3e9477e0f24b1
Marcel Kyas <kyas@localhost.localdomain>
2009-05-08 15:02:19 +0200

    Also record the branch in the version information.

--- creolc/Driver.ml
+++ creolc/Driver.ml
@@ -123,7 +123,8 @@ let license =
 let show_version () =
   (** Show the name and the version of the program and exit. *)
   print_string (Version.package ^ " " ^ Version.version ^ " (" ^
-		   Version.release ^ " of " ^ Version.reldate ^ ")\n" );
+		   Version.release ^ Version.branch ^ " of " ^
+                   Version.reldate ^ ")\n");
   print_string license ;
   exit 0
 
