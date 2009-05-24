b85283e51cb6f3b30a777a87e4408d310e356082
Marcel Kyas <kyas@localhost.localdomain>
2009-05-24 15:28:17 +0200

    Arrange for prettier output of object terms.

--- creolc/BackendCreol.ml
+++ creolc/BackendCreol.ml
@@ -607,43 +607,29 @@ let pretty_print_program out_channel input =
     close_box () ;
     open_vbox 0 ;
     if [] <> obj.Object.attributes then
-      begin
-        open_vbox 0 ;
-	print_vardecls "var " print_space obj.Object.attributes;
-	close_box () ;
-        print_space () ;
-        print_space ()
-      end ;
-    begin
-      match obj.Object.process with
-	| { Process.attributes = []; code = (Statement.Skip _) } ->
-	    open_box 2 ;
-	    print_string "No active process." ;
-	    close_box ()
-	| { Process.attributes = []; code = c } ->
-	    open_box 2 ;
-	    print_string "Active Process:\n" ;
-	    open_box 2 ;
-	    print_statement c ;
-	    close_box () ;
-	    close_box ()
-	| { Process.attributes = a; code = c } ->
-	    open_box 2 ;
-	    print_string "Active Process:\n" ;
-	    open_box 2 ;
-	    print_vardecls "var " print_space a ;
-	    close_box ();
-	    open_box 2 ;
-	    print_statement c ;
-	    close_box () ;
-	    close_box ()
-    end ;
+	print_vardecls "var " print_space obj.Object.attributes ;
+    print_space () ;
+    print_string "Active Process:" ;
+    print_space () ;
+    open_hbox () ;
+    print_space () ;
+    print_space () ;
+    close_box () ;
+    open_vbox 0 ;
+    print_process obj.Object.process ;
+    close_box () ;
+    print_space () ;
     if [] <> obj.Object.process_queue then
       begin
-	open_box 2 ;
-	print_string "Process Queue:\n" ;
-	List.iter print_process obj.Object.process_queue ;
-	close_box ()
+	print_string "Process Queue:" ;
+        print_space () ;
+        open_hbox () ;
+        print_space () ;
+        print_space () ;
+        close_box () ;
+        open_vbox 0;
+	separated_list print_process print_space obj.Object.process_queue ;
+        close_box ()
       end ;
     close_box () ;
     print_space () ;
@@ -652,24 +638,15 @@ let pretty_print_program out_channel input =
   and print_process =
     function 
       | { Process.attributes = []; code = (Statement.Skip _) } ->
-	print_string "idle"
+        open_hbox () ; print_string "idle" ; close_box ()
       | { Process.attributes = []; code = c } ->
-	open_box 2 ;
-	print_string "Process:\n" ;
-	open_box 2 ;
 	print_statement c ;
-	close_box () ;
-	close_box ()
+        print_space ()
       | { Process.attributes = a; code = c } ->
-	open_box 2 ;
-	print_string "Process:\n" ;
-	open_box 2 ;
 	print_vardecls "var " print_space a;
-	close_box () ;
-	open_box 2 ;
+        print_space () ;
 	print_statement c;
-	close_box () ;
-	close_box ()
+        print_space ()
   and print_future f =
     open_box 2 ;
     print_string "Future" ;
@@ -871,10 +848,11 @@ let pretty_print_program out_channel input =
     close_box ()
   and print_vardecls prefix delimiter vardecls =
     let print vardecl =
-      open_box 2 ; print_string prefix; print_vardecl vardecl ; close_box ()
+      print_string prefix; print_vardecl vardecl
     in
       separated_list print delimiter vardecls
   and print_vardecl v =
+    open_box 2 ;
     print_string v.VarDecl.name ;
     print_string ":" ;
     print_space () ;
@@ -886,10 +864,9 @@ let pretty_print_program out_channel input =
 	    print_space () ;
 	    print_string ":=" ;
 	    print_space () ;
-	    open_box 2 ;
 	    print_expression e ;
-	    close_box ()
-    end
+    end ;
+    close_box ()
   and print_pragma { Pragma.name = n; values = v } =
     print_string n ;
     if List.length v > 0 then
